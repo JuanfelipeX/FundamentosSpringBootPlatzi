f97f47ae8105650c1fad4ac9deb9a7c9743c54c0
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2022-02-23 11:12:23 -0500

    Uso de properties y valores 14/31

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
@@ -1,6 +1,7 @@
 package com.fundamentosplatzi.springboot.fundamentos;
 
 import com.fundamentosplatzi.springboot.fundamentos.bean.MyBean;
+import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanWithProperties;
 import com.fundamentosplatzi.springboot.fundamentos.component.ComponentDependency;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
@@ -12,16 +13,20 @@ public class FundamentosApplication implements CommandLineRunner {
 
 	private ComponentDependency componentDependency;
 	private MyBean myBean;
+	private MyBeanWithProperties myBeanWithProperties;
 
-	public FundamentosApplication(ComponentDependency componentDependency, MyBean myBean){
+	public FundamentosApplication(ComponentDependency componentDependency, MyBean myBean, MyBeanWithProperties myBeanWithProperties){
 		this.componentDependency = componentDependency;
 		this.myBean = myBean;
+		this.myBeanWithProperties = myBeanWithProperties;
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
 		componentDependency.saludar();
 		myBean.print();
+		System.out.println(myBeanWithProperties.function());
+
 	}
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/bean/MyBeanWithPropertiesImplements.java
@@ -0,0 +1,17 @@
+package com.fundamentosplatzi.springboot.fundamentos.bean;
+
+public class MyBeanWithPropertiesImplements implements MyBeanWithProperties{
+
+    private String nombre;
+    private String apellido;
+
+    public MyBeanWithPropertiesImplements(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    @Override
+    public String function() {
+        return nombre + " - " + apellido;
+    }
+}
