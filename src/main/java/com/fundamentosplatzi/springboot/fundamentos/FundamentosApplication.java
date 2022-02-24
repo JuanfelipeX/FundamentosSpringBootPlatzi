2afca5ca3834bc1203c5e31777affa0be85367fd
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2022-02-24 11:46:36 -0500

    Qué son los logs y cómo usarlos 16/31

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
@@ -4,6 +4,8 @@ import com.fundamentosplatzi.springboot.fundamentos.configuration.bean.MyBean;
 import com.fundamentosplatzi.springboot.fundamentos.configuration.bean.MyBeanWithProperties;
 import com.fundamentosplatzi.springboot.fundamentos.component.ComponentDependency;
 import com.fundamentosplatzi.springboot.fundamentos.pojo.UserPojo;
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,9 +14,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FundamentosApplication implements CommandLineRunner {
 
 	private ComponentDependency componentDependency;
+
 	private MyBean myBean;
+
 	private MyBeanWithProperties myBeanWithProperties;
+
 	private UserPojo userPojo;
+
+	private final Log LOGGER = LogFactory.getLog(FundamentosApplication.class);
 
 	public FundamentosApplication(ComponentDependency componentDependency, MyBean myBean, MyBeanWithProperties myBeanWithProperties, UserPojo userPojo){
 		this.componentDependency = componentDependency;
@@ -29,6 +36,13 @@ public class FundamentosApplication implements CommandLineRunner {
 		myBean.print();
 		System.out.println(myBeanWithProperties.function());
 		System.out.println(userPojo.getEmail() + " - " + userPojo.getPassword());
+
+
+		try {
+			//code
+		} catch (Exception e) {
+			LOGGER.error("Esto es un error del aplicativo");
+		}
 	}
 
 	public static void main(String[] args) {
