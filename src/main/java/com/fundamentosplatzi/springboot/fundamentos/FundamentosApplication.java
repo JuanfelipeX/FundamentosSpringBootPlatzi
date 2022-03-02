caae3a087eccaf3f226934f7dd1de2a73e05f07c
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2022-03-02 12:16:37 -0500

    Registro en base de datos con JpaRepository 19/31

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
@@ -3,12 +3,19 @@ package com.fundamentosplatzi.springboot.fundamentos;
 import com.fundamentosplatzi.springboot.fundamentos.configuration.bean.MyBean;
 import com.fundamentosplatzi.springboot.fundamentos.configuration.bean.MyBeanWithProperties;
 import com.fundamentosplatzi.springboot.fundamentos.component.ComponentDependency;
+import com.fundamentosplatzi.springboot.fundamentos.entity.User;
 import com.fundamentosplatzi.springboot.fundamentos.pojo.UserPojo;
+import com.fundamentosplatzi.springboot.fundamentos.repository.UserRepository;
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.lang.reflect.Array;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class FundamentosApplication implements CommandLineRunner {
@@ -23,30 +30,49 @@ public class FundamentosApplication implements CommandLineRunner {
 
 	private final Log LOGGER = LogFactory.getLog(FundamentosApplication.class);
 
-	public FundamentosApplication(ComponentDependency componentDependency, MyBean myBean, MyBeanWithProperties myBeanWithProperties, UserPojo userPojo){
+	private UserRepository userRepository;
+
+	public FundamentosApplication(ComponentDependency componentDependency, MyBean myBean, MyBeanWithProperties myBeanWithProperties, UserPojo userPojo, UserRepository userRepository){
 		this.componentDependency = componentDependency;
 		this.myBean = myBean;
 		this.myBeanWithProperties = myBeanWithProperties;
 		this.userPojo = userPojo;
+		this.userRepository = userRepository;
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		//ejemplosAnteriores();
+		saveUserInDataBase();
+
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosApplication.class, args);
+	}
+
+	private void saveUserInDataBase(){
+		User user1 = new User("jhon", "john69@konrda.com", LocalDate.of(2022, 03, 03));
+		User user2 = new User("felipe", "felipe@gmail.com", LocalDate.of(2021, 23, 02));
+		User user3 = new User("jimene", "jimene@yahoo.com", LocalDate.of(2024, 12, 05));
+		User user4 = new User("pacheco", "pacheco@konra.com", LocalDate.of(2024, 04, 04));
+
+		List<User> list = Arrays.asList(user1, user2, user3, user4);
+		list.stream().forEach(userRepository::save);
+
+	}
+
+	private void ejemplosAnteriores(){
 		componentDependency.saludar();
 		myBean.print();
 		System.out.println(myBeanWithProperties.function());
 		System.out.println(userPojo.getEmail() + " - " + userPojo.getPassword());
-
 
 		try {
 			//code
 		} catch (Exception e) {
 			LOGGER.error("Esto es un error del aplicativo");
 		}
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(FundamentosApplication.class, args);
 	}
 
 }
